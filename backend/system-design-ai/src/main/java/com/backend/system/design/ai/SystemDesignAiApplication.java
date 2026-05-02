7668dd09bda179b357044d671db88b5396848d9c
UMANG AGRAWAL <umangagrawal@UMANGs-MacBook-Air.local>
2026-05-02 21:52:21 +0530

    chore: initial project setup with Spring Boot and dependencies

---
+++ backend/system-design-ai/src/main/java/com/backend/system/design/ai/SystemDesignAiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.system.design.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignAiApplication.class, args);
+	}
+
+}
---
+++ backend/system-design-ai/src/test/java/com/backend/system/design/ai/SystemDesignAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.system.design.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
