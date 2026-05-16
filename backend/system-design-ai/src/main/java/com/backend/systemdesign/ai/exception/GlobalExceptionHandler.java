fde864ffc9ad95be700bb96a7572cfebebdde998
UMANG AGRAWAL <umangagrawal@UMANGs-MacBook-Air.local>
2026-05-16 16:13:35 +0530

    feat: add standardized error response handling for APIs

--- backend/system-design-ai/src/main/java/com/backend/systemdesign/ai/exception/GlobalExceptionHandler.java
+++ backend/system-design-ai/src/main/java/com/backend/systemdesign/ai/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.backend.systemdesign.ai.exception;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.backend.systemdesign.ai.dto.response.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,10 +25,16 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound( ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleResourceNotFound( ResourceNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(
+                                    LocalDateTime.now(),
+                                    HttpStatus.NOT_FOUND.value(),
+                                    ex.getMessage()
+                            );
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(error);
     }
 }
